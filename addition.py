50df2d53a3c45d0c30fd1c71113c1f8c772db8dd
naveenef45 <naveenef45@gmail.com>
2022-03-18 13:43:40 +0530

    one newfile is created addition .py

---
+++ addition.py
@@ -0,0 +1,14 @@
+class Addition
+{
+public static void main(string args[])
+{
+    int num1,num2,res;
+    num1=20:
+    num2=45:
+    res=num1+num2;
+    system.out.println("the result is",+res);
+    }
+}
+
+
+    
